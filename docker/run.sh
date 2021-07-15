b07008b2af3e37a3a158d591fabb2df94b93169a
Kostia Onofreichuk <2535925+olndr@users.noreply.github.com>
2021-07-15 18:23:57 +0300

    updated host address

--- docker/run.sh
+++ docker/run.sh
@@ -12,4 +12,4 @@ bundle exec rake db:migrate
 #bundle exec rake db:create
 #bundle exec rake db:migrate
 bundle exec rake db:seed
-bundle exec rackup --port 80 --host localhost
+bundle exec rackup --port 80 --host 0.0.0.0
